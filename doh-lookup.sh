881f17b180cadcbefc81fe15a23adfce183b0ec9
Abdelhalim38 <37987972+Abdelhalim38@users.noreply.github.com>
2024-11-07 16:35:05 +0100

    Update doh-lookup.sh

--- doh-lookup.sh
+++ doh-lookup.sh
@@ -12,7 +12,7 @@
 #
 export LC_ALL=C
 export PATH="/usr/sbin:/usr/bin:/sbin:/bin"
-input1="https://raw.githubusercontent.com/Abdelhalim38/hosts/refs/heads/master/more.txt"
+input1="./hosts/more.txt"
 input2="input2.txt"
 input3="input3.txt"
 upstream="8.8.8.8"
